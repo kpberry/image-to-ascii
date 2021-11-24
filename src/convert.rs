d7379c0a224538a2ebef782ec8d85c38f5b84fec
Kevin Berry <kpberry11@gmail.com>
2021-11-24 02:36:16 -0500

    Add fast conversion algorithm

--- src/convert.rs
+++ src/convert.rs
@@ -70,8 +70,9 @@ pub fn pixels_to_ascii(font: &Font, pixels: Vec<f32>, metric: Metric,
             chars.extend(char_vec);
         }
     } else {
-         chars = chunks.iter()
-                    .map(|chunk| pixel_chunk_to_ascii(&font, chunk, metric)).collect();
+        // TODO make pixel_chunk_to_ascii a parameter so that "fast" can be passed in
+        chars = chunks.iter()
+            .map(|chunk| pixel_chunk_to_ascii(&font, chunk, metric)).collect();
     }
 
     let mut char_rows: Vec<Vec<char>> = Vec::new();
@@ -109,4 +110,68 @@ pub fn img_to_ascii(font: &Font, img: &DynamicImage, metric: Metric, out_width: 
     pixels = pixels.iter().map(|x| (x - pixels_mean) / pixels_mean).collect();
 
     pixels_to_ascii(font, pixels, metric, resize_width, resize_height, out_width, out_height, n_threads)
+}
+
+pub fn pixels_to_ascii_fast(font: &Font, pixels: Vec<f32>,
+                            width: usize, height: usize,
+                            out_width: usize, out_height: usize) -> String {
+    let chunk_size = font.width * font.height;
+    let vertical_chunks = height / font.height;
+    let horizontal_chunks = width / font.width;
+
+    // not the simplest way of doing this, but should give good cache performance and avoids
+    // multiplications/divisions
+    let mut chunks: Vec<Vec<f32>> = Vec::with_capacity(vertical_chunks * horizontal_chunks);
+    let mut y_offset = 0;
+    let mut x_offset = 0;
+    for _ in 0..vertical_chunks {
+        let mut chunk_row: Vec<Vec<f32>> = (0..horizontal_chunks)
+            .map(|_| Vec::with_capacity(chunk_size)).collect();
+
+        for _ in 0..font.height {
+            for x in 0..horizontal_chunks {
+                let start = y_offset + x_offset;
+                let end = start + font.width;
+                let chunk_pixel_row = &pixels[start..end];
+                chunk_row[x].extend(chunk_pixel_row);
+                x_offset += font.width;
+            }
+            y_offset += width;
+            x_offset = 0;
+        }
+
+        chunks.extend(chunk_row);
+    }
+
+    let chars: Vec<char> = chunks.iter()
+        .map(|chunk| {
+            let intensity = chunk.iter().sum::<f32>() as usize;
+            font.intensity_chars[intensity].value
+        })
+        .collect();
+
+    let mut char_rows: Vec<Vec<char>> = Vec::new();
+    for j in 0..out_height {
+        let start = j * out_width;
+        let end = start + out_width;
+        let row = chars[start..end].iter().cloned().collect();
+        char_rows.push(row);
+    }
+    let strings: Vec<String> = char_rows.iter().map(|chars| chars.iter().collect()).collect();
+    let result = strings.join("\n");
+
+    result
+}
+
+pub fn img_to_ascii_fast(font: &Font, img: &DynamicImage, out_width: usize) -> String {
+    let (width, height) = img.dimensions();
+
+    let resize_width = out_width * font.width;
+    let out_height = (((resize_width * height as usize) as f64) / ((width as usize * font.height) as f64)).round() as usize;
+    let resize_height = out_height * font.height;
+    let img = img.resize_exact(resize_width as u32, resize_height as u32, FilterType::Triangle);
+    let img = img.to_luma8();
+
+    let mut pixels: Vec<f32> = img.pixels().map(|&Luma([x])| x as f32 / 255.).collect();
+    pixels_to_ascii_fast(font, pixels, resize_width, resize_height, out_width, out_height)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 use std::env;
 use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 use crate::font::Font;
 use crate::metrics::{avg_color_score, dot_score, jaccard_score, Metric, movement_toward_clear, occlusion_score};
 
@@ -10,7 +12,6 @@ mod gif;
 mod convert;
 
 fn main() {
-    let mut font = Font::from_bdf(Path::new("fonts/kourier.bdf"));
     let alphabet = vec![
         ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')',
         '*', '+', ',', '-', '.', '/', '0', '1', '2', '3',
@@ -21,43 +22,55 @@ fn main() {
         '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
         'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
         'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{',
-        '|', '}', '~'
+        '|', '}', '~',
     ];
-    let char_set: HashSet<char> = alphabet.iter().cloned().collect();
-    font.chars = font.chars.iter().filter(|c| char_set.contains(&c.value)).cloned().collect();
+    let mut font = Font::from_bdf(Path::new("fonts/kourier.bdf"), alphabet);
 
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
+    let path = Path::new(filename);
+    let extension = path.extension().unwrap();
 
     let default_width = String::from("150");
     let width = args.get(2).unwrap_or(&default_width).parse::<usize>().unwrap();
 
     let default_metric = String::from("dot");
     let metric_str = &args.get(3).unwrap_or(&default_metric)[..];
-    let metric: Option<Metric> = match metric_str {
-        "jaccard" => Some(jaccard_score),
-        "dot" => Some(dot_score),
-        "occlusion" => Some(occlusion_score),
-        "color" => Some(avg_color_score),
-        "clear" => Some(movement_toward_clear),
-        _ => None
-    };
-    // if the user specified a metric, don't fall back to the default
-    let metric = metric.expect(&format!("Unsupported metric {}", metric_str));
-
-    let path = Path::new(filename);
-    let extension = path.extension().unwrap();
-
-    if extension == "gif" {
-        let gif = gif::read_gif(path);
-        for img in gif {
-            let ascii = convert::img_to_ascii(&font, &img, metric,width, 8);
-            println!("{}[2J{}", 27 as char, ascii);
+    if metric_str == "fast" {
+        if extension == "gif" {
+            let gif = gif::read_gif(path);
+            for img in gif {
+                let ascii = convert::img_to_ascii_fast(&font, &img, width);
+                println!("{}[2J{}", 27 as char, ascii);
+            }
+        } else {
+            let img = image::open(path).unwrap();
+            let ascii = convert::img_to_ascii_fast(&font, &img, width);
+            println!("{}", ascii);
         }
     } else {
-        let img = image::open(path).unwrap();
-        let ascii = convert::img_to_ascii(&font, &img, metric,width, 8);
-        println!("{}", ascii);
+        let metric: Option<Metric> = match metric_str {
+            "jaccard" => Some(jaccard_score),
+            "dot" => Some(dot_score),
+            "occlusion" => Some(occlusion_score),
+            "color" => Some(avg_color_score),
+            "clear" => Some(movement_toward_clear),
+            _ => None
+        };
+        // if the user specified a metric, don't fall back to the default
+        let metric = metric.expect(&format!("Unsupported metric {}", metric_str));
+
+        if extension == "gif" {
+            let gif = gif::read_gif(path);
+            for img in gif {
+                let ascii = convert::img_to_ascii(&font, &img, metric, width, 8);
+                println!("{}[2J{}", 27 as char, ascii);
+            }
+        } else {
+            let img = image::open(path).unwrap();
+            let ascii = convert::img_to_ascii(&font, &img, metric, width, 8);
+            println!("{}", ascii);
+        }
     }
 }
