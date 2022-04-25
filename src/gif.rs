c028bd59b0ca9557f04df6d6e236d0563d279e99
Kevin Berry <kpberry11@gmail.com>
2022-04-25 01:20:03 -0400

    Add function to read gif from stream

--- src/gif.rs
+++ src/gif.rs
@@ -1,15 +1,20 @@
 use image::codecs::gif::GifDecoder;
 use image::{AnimationDecoder, DynamicImage};
 use std::fs::File;
+use std::io::Read;
 use std::path::Path;
 
-pub fn read_gif(path: &Path) -> Vec<DynamicImage> {
-    let fp = File::open(path).unwrap();
-    let decoder = GifDecoder::new(fp).unwrap();
+pub fn read_gif_from_stream<R: Read>(stream: R) -> Vec<DynamicImage> {
+    let decoder = GifDecoder::new(stream).unwrap();
     let frames = decoder.into_frames();
     let frames = frames.collect_frames().expect("error decoding gif");
     frames
         .iter()
         .map(|frame| DynamicImage::ImageRgba8(frame.buffer().clone()))
         .collect()
+}
+
+pub fn read_gif(path: &Path) -> Vec<DynamicImage> {
+    let fp = File::open(path).unwrap();
+    read_gif_from_stream(fp)
 }
